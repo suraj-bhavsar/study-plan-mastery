5e68c9ad2aaf0c592718ecb34ed955da85a01e68
suraj-bhavsar <suraj@questt.com>
2023-09-06 17:26:23 +0530

    UPDATE: Firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:bot_toast/bot_toast.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:study_plan_student_dashboard/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final botToastBuilder = BotToastInit();
   runApp(
     GetMaterialApp(
